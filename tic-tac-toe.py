cf918f20363e459aad051ab0cb16d69177912d1d
ramon <monchouam@gmail.com>
2022-04-26 18:05:53 -0500

    added a first strategy to look for a winner by row

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,76 +10,96 @@ If all nine squares are full and neither player has three in a row, the game end
 
 """
 
-def create_grid():
-    """
-    Returns the grid to perform the game
-    
-    input:
-        Nothing
-    output:
-        grid: (str) the grid
-    """
-    grid = f"""
-1|2|3
--+-+-
-4|5|6
--+-+-
-7|8|9 """
-    return grid
 
-def put_answer(grid, value, position):
+
+from numpy import matrix
+
+
+def show_matrix(matrix):
     """
-    Given a grid puts the input value in the indicated position
+    Given a list of list (matrix) return a formated string with the matrix values:
+    1|2|3
+    -+-+-
+    4|5|6
+    -+-+-
+    7|8|9
 
     input:
-        grid: (str) the grid to play
-        value: (str) the value that a gamer is going to put on the grid (x or o)
-        position: (str) the number of the position where the gamer wants to put the respective value
+        matrix: (list of list [int]) list of list with the int numbers
     """
-    modified_grid = grid.replace(position, value)
-    return modified_grid
 
-def matrix():
-    m = [[1,2,3], [4,5,6], [7,8,9]]
-    fi = len(m)
-    co = len(m[0])
+    # matrix = [[1,2,3], [4,5,6], [7,8,9]]
+    fi = len(matrix)
+    co = len(matrix[0])
     for i in range(fi):
         for j in range(co):
             if j == co - 1: 
-                print(m[i][j], end="")
+                print(matrix[i][j], end="")
             else:
-                print(m[i][j], end="|")            
+                print(matrix[i][j], end="|")            
         print()
         if i != fi - 1:
-            print("-+-+-")
+            print("".join(["-+-"] * (fi -1)))
         else:
             print("")
 
-def array():
-    m = [[1,2,3], [4,5,6], [7,8,9]]
-    n = 6
-    # pos = m[1].index(n)
-    # print(pos)
 
-    for row_index, row in enumerate(m):
+
+def find_position(matrix, n):
+    found_row = None
+    found_col = None
+
+    for row_index, row in enumerate(matrix):        
         try:
-            pos = row.index(n)
-            return row_index, pos
+            pos = row.index(n)            
+            found_row = row_index
+            found_col = pos
+            break
         except:
-            pass    
+            continue
+    return found_row, found_col  
     
+def put_value(matrix, i, j, value):
+    matrix[i][j] = value
+   
+
+def ask_input(player):
+    return int(input(f"{player}'s turn to choose a square (1-9): "))
+
+
+def win_by_row(matrix):
+    won = False
+    for row in matrix:
+        to_compare = "".join(map(str, row))
+        if to_compare == "xxx" or to_compare == "ooo":
+            won = True
+            break
+        else:
+            continue
+    return won
+        
+
 
 def main():
-    # grid = create_grid()    
-    # modified_grid = put_answer(grid, "x", "2")
-    # print(modified_grid)
-    # m1 = modified_grid.split("\n")
-    # print(m1)
-    # print(len(m1))
-    # print()
-    matrix()
-    i, j = array()
-    print(f"[{i}, {j}]")
+    m = [[1,2,3], [4,5,6], [7,8,9]]
+    show_matrix(m)
+
+    won = False
+    count = 1
+    while won == False:
+        if count % 2 == 0:
+            player = "o"
+        else:
+            player = "x"
+        selected = ask_input(player)
+        
+        i, j = find_position(m, selected)
+        if i != None and j != None:
+            put_value(m, i, j, player)
+            won = win_by_row(m)
+        show_matrix(m)
+    
+        count += 1
 
 
 
